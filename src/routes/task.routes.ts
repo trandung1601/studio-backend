48e73452aea8a5dc0f0814e15b6e260d2ebdce7b
Dung <trantiendung1601@gmail.com>
2025-06-16 12:05:23 +1000

    Update task.routes.ts

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,8 +1,11 @@
 import express from "express";
+
 import { getTasks } from "../controllers/task.controller";
 
 const router = express.Router();
 
+
+// Task routes
 router.get("/hello", getTasks);
 
 export default router;
